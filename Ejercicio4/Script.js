b3ff1917f01cc9ccb294fecf78fc3b2b97eb4d8a
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-15 21:51:12 -0500

    Implementando variable chart para la ejecución de la gráfica

--- Ejercicio4/Script.js
+++ Ejercicio4/Script.js
@@ -10,6 +10,26 @@ document.getElementById("comparasion").addEventListener("submit", function (even
 
       const fechas = datosRegion.confirmed.map(entry => entry.date);
       const valoresRegion = datosRegion.confirmed.map(entry => parseInt(entry.value)); 
+      //caso contrario la crea
+      const ctx = document.getElementById('grafica').getContext('2d');
+      let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: fechas,
+          datasets: [
+            {
+              label: region,
+              data: valoresRegion,
+              borderColor: 'blue',
+              backgroundColor: 'rgba(0, 0, 255, 0.1)'
+            },
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      });
     })
   .catch(error => console.error('Error:', error));
 });
