e69753a12ef1eccd33dcb87084f81f248c24301d
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-15 20:15:35 -0500

    Ejercicio 3, Agregando for para que itere sobre los datos , y este mediante un mapa se iran almacenando la region y la suma

---
+++ Ejercicio3/Ejercicio3.js
@@ -0,0 +1,23 @@
+import('node-fetch').then(async (module) => {
+  const fetch = module.default;
+  const response = await fetch('http://localhost/nuevo/JASON/data.json');
+  let mapa = new Map();
+  if (response.ok) {
+    const data = await response.json();
+    console.log("\t Lista de Regiones");
+    //Agregar datos tanto suma como region a un mapa
+    for (let i = 0; i < data.length; i++) {
+      let confirmados = data[i].confirmed;
+      let region = data[i].region;
+      let suma = 0;
+      for(let c = 0; c < confirmados.length; c++){
+        suma += parseInt(confirmados[c].value);
+      }
+      mapa.set(region,suma);
+    }
+  } else {
+    console.error('Error en la solicitud:', response.status);
+  }
+}).catch(error => {
+  console.error('Error:', error);
+});
