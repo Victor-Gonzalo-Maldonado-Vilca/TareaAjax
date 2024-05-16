743d79d9b9a8d104e86d6ff1866f97e2b4998316
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-15 19:15:50 -0500

    Agregacion solicitud de la API fetch para obtener datos de regiones desde un archivo JSON

--- Ejercicio1/Ejercicio1.js
+++ Ejercicio1/Ejercicio1.js
@@ -1,6 +1,18 @@
 import('node-fetch').then(async (module) => {
   const fetch = module.default;
     const response = await fetch('http://localhost/nuevo/JASON/data.json');
+    // Verifica si la respuesta fue exitosa
+    if (response.ok) {
+        const data = await response.json();
+        console.log("\t Lista de Regiones");
+        //Itera sobre los datos obtenidos e imprime las regiones
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].region);
+        }
+    } else {
+        console.error('Error en la solicitud:', response.status);
+    }
+//Captura de error
 }).catch(error => {
     console.error('Error:', error);
 });
